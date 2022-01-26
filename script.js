24cff41b04119b85c0f825802cbb1d7d0dd222c1
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-01-26 23:04:08 +0000

    Search dropdown top

---
+++ script.js
@@ -0,0 +1,12 @@
+// https://www.w3schools.com/howto/howto_js_dropdown.asp
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+function menuDropdown() {
+    document.getElementById("menuDropdown").classList.toggle('show');
+}
+
+function searchDropdown() {
+    document.getElementById("searchDropdown").classList.toggle('show');
+}
+
