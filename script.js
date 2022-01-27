873e9e6713d8b1d59c4307d47eb269dd36f3d5e1
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-01-27 01:32:57 +0000

    Search dropdown finished

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ function searchDropdown() {
     document.getElementById("searchDropdown").classList.toggle('show');
 }
 
+function overlay() {
+    document.getElementById("overlay").classList.toggle('onoff');
+}
+
+function siteDropdown() {
+    document.getElementById("siteDropdown").classList.toggle('display');
+}
+
+function siteFocusCom() {
+    document.getElementById("playStationCom").classList.toggle('focus');
+    document.getElementById("playStationStore").classList.remove('focus');
+    document.getElementById("searchField").placeholder = "Search PlayStation.com";
+}
+
+function siteFocusStore() {
+    document.getElementById("playStationStore").classList.toggle('focus');
+    document.getElementById("playStationCom").classList.remove('focus');
+    document.getElementById("searchField").placeholder = "Search PS Store";
+}
+
+document.getElementById("menuDropBtn").addEventListener("click", menuDropdown);
+document.getElementById("menuDropBtn").addEventListener("click", overlay);
+
+document.getElementById("searchDropBtn").addEventListener("click", searchDropdown);
+document.getElementById("searchDropBtn").addEventListener("click", overlay);
+
+document.getElementById("searchBackBtn").addEventListener("click", searchDropdown);
+document.getElementById("searchBackBtn").addEventListener("click", overlay);
+
+document.getElementById("siteDropBtn").addEventListener("click", siteDropdown);
+
+document.getElementById("siteBackBtn").addEventListener("click", siteDropdown);
+
+document.getElementById("playStationCom").addEventListener("click", siteDropdown);
+document.getElementById("playStationCom").addEventListener("click", siteFocusCom);
+
+document.getElementById("playStationStore").addEventListener("click", siteDropdown);
+document.getElementById("playStationStore").addEventListener("click", siteFocusStore);
