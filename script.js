b15f882f53e1507bad9356d72100ff93faccb88b
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-01-28 22:47:34 +0000

    building dropdown

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+
 // https://www.w3schools.com/howto/howto_js_dropdown.asp
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -8,6 +24,26 @@ function menuDropdown() {
 
 function gamesSideMenu() {
     document.getElementById("gamesMenu").classList.toggle('slidein');
+}
+
+function hardwareSideMenu() {
+    document.getElementById("hardwareMenu").classList.toggle('slidein');
+}
+
+function servicesSideMenu() {
+    document.getElementById("servicesMenu").classList.toggle('slidein');
+}
+
+function newsSideMenu() {
+    document.getElementById("newsMenu").classList.toggle('slidein');
+}
+
+function shopSideMenu() {
+    document.getElementById("shopMenu").classList.toggle('slidein');
+}
+
+function supportSideMenu() {
+    document.getElementById("supportMenu").classList.toggle('slidein');
 }
 
 function gamesSideMenuOff() {
@@ -46,6 +82,16 @@ document.getElementById("menuDropBtn").addEventListener("click", menuDropdown);
 document.getElementById("menuDropBtn").addEventListener("click", gamesSideMenuOff)
 
 document.getElementById("games").addEventListener("click", gamesSideMenu);
+
+document.getElementById("hardware").addEventListener("click", hardwareSideMenu);
+
+document.getElementById("services").addEventListener("click", servicesSideMenu);
+
+document.getElementById("news").addEventListener("click", newsSideMenu);
+
+document.getElementById("shop").addEventListener("click", shopSideMenu);
+
+document.getElementById("support").addEventListener("click", supportSideMenu);
 
 document.getElementById("gamesMenuTitle").addEventListener("click", gamesSideMenuOff);
 
