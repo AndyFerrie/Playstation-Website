f27dee7a34f6dd43cde21e813b0bf7b53d35cb31
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-01-29 00:57:41 +0000

    dropdown menu

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
-  });
-}
-
-// https://www.w3schools.com/howto/howto_js_dropdown.asp
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-
 function menuDropdown() {
     document.getElementById("menuDropdown").classList.toggle('display');
 }
